a1801a0f3263cd3fe91cde6997726feb725c3ddf
almostgone47 <almostgone47@hotmail.com>
2023-08-31 17:01:21 +0700

    add files to gitignore

--- tests/cmake-build-debug/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jeremiah/CLionProjects/planters/googletest
-# Build directory: /Users/jeremiah/CLionProjects/planters/tests/cmake-build-debug/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- tests/cmake-build-debug/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jeremiah/CLionProjects/planters/googletest/googlemock
-# Build directory: /Users/jeremiah/CLionProjects/planters/tests/cmake-build-debug/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
